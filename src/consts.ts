608d156223b875b91b9656ffe3e1130fefdd17e6
sotelors <rubenalfonsonic@hotmail.com>
2025-02-02 00:06:28 -0600

    Primer commit - Astro app

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'GSB Innovations';
+export const SITE_DESCRIPTION = 'Bienvenido a nuestro sitio web!';
